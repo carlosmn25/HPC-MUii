9814ebb2e23069815c0aaff23446cc3843b3fac2
cmedrano <carlos.medrano@uclm.es>
2023-03-02 16:12:19 +0100

    Cambios finales del lab1

--- lab1/seq_ijk.c
+++ lab1/seq_ijk.c
@@ -23,15 +23,22 @@ int main(int argc, char *argv[]) {
     B_FILAS = atoi(argv[3]);
     B_COLUMNAS = atoi(argv[4]);
 
+    printf("%d %d %d %d\n", A_FILAS, A_COLUMNAS, B_FILAS, B_COLUMNAS);
+
     //int A[A_FILAS][A_COLUMNAS], B[B_FILAS][B_COLUMNAS], C[A_FILAS][B_COLUMNAS];
     //initialize the matrixes with malloc
     int *A = (int *)malloc(A_FILAS * A_COLUMNAS * sizeof(int));
     int *B = (int *)malloc(B_FILAS * B_COLUMNAS * sizeof(int));
     int *C = (int *)malloc(A_FILAS * B_COLUMNAS * sizeof(int));
 
+    if(A == NULL || B == NULL || C == NULL) {
+        printf("Error: No se pudo reservar memoria para las matrices\n");
+        exit(1);
+    }
+
     if(A_COLUMNAS != B_FILAS) {
-        printf("No se pueden multiplicar las matrices");
-        return 0;
+        printf("No se pueden multiplicar las matrices\n");
+        exit(2);
     }
 
     for (i = 0; i < A_FILAS; i++) {
@@ -54,7 +61,7 @@ int main(int argc, char *argv[]) {
 
     clock_t start = clock();
 
-    //Multiplicacion de matrices
+    //Multiplicacion de matrices ijk
     for (i = 0; i < A_FILAS; i++) {
         for (j = 0; j < A_COLUMNAS; j++) {
             for (k = 0; k < B_COLUMNAS; k++) {
@@ -76,5 +83,42 @@ int main(int argc, char *argv[]) {
     }*/
 
     printf("Tiempo de ejecucion: %f segundos \n", (double)(end - start) / CLOCKS_PER_SEC);
-    
+
+    clock_t start2 = clock();
+
+    //Multiplicacion de matrices jki
+    for (j = 0; j < A_COLUMNAS; j++) {
+        for (k = 0; k < B_COLUMNAS; k++) {
+            for (i = 0; i < A_FILAS; i++) {
+                //C[i] += A[i] * B[k];
+                C[B_COLUMNAS*i + k] += A[A_COLUMNAS*i + j] * B[B_COLUMNAS*j + k];
+            }
+        }
+    }
+
+    clock_t end2 = clock();
+
+    printf("Tiempo de ejecucion: %f segundos \n", (double)(end2 - start2) / CLOCKS_PER_SEC);
+
+    clock_t start3 = clock();
+
+    //Multiplicacion de matrices kji
+    for (k = 0; k < B_COLUMNAS; k++) {
+        for (j = 0; j < A_COLUMNAS; j++) {
+            for (i = 0; i < A_FILAS; i++) {
+                //C[i] += A[i] * B[k];
+                C[B_COLUMNAS*i + k] += A[A_COLUMNAS*i + j] * B[B_COLUMNAS*j + k];
+            }
+        }
+    }
+
+    clock_t end3 = clock();
+
+    printf("Tiempo de ejecucion: %f segundos \n", (double)(end3 - start3) / CLOCKS_PER_SEC);
+
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
 }
