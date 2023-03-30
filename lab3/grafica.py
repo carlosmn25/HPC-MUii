2e98969a9541291ba78e2724a6dbfcc5b18b13f5
ajtarraga <antoniojoaquin231999@gmail.com>
2023-03-30 19:02:12 +0200

    Efficiency and Speed-up

--- lab3/grafica.py
+++ lab3/grafica.py
@@ -10,10 +10,28 @@ time = df['time']
 num = df['nthreads']
 time_loop = df['loop']
 
+efficiency = []
+efficiency_loop = []
+
 fig = plt.plot(num, time)
 plt.xlabel('Number of threads')
 plt.ylabel('Time in seconds')
 plt.title('Time in seconds vs Number of threads')
+plt.xticks(num)
+
+# Show the speedup of each xticks
+for i, v in enumerate(time):
+    # If i is power of 2
+    if i in {0,1,3,7,15}:
+        plt.text(num[i], v, str(round(time[0]/v, 2)) + 'x', color='blue', fontweight='bold', position=(num[i]-2, v))
+
+# Show the efficiency of each xticks
+for i, v in enumerate(time):
+    efficiency.append(time[0]/(v*num[i]))
+    # If i is power of 2
+    if i in {0,1,3,7,15}:
+        plt.text(num[i], v, str(round(time[0]/(v*num[i]), 2)) + 'x', color='red', fontweight='bold')
+
 
 plt.savefig('lab3/figTotal.png')
 
@@ -23,5 +41,44 @@ fig = plt.plot(num, time_loop)
 plt.xlabel('Number of threads')
 plt.ylabel('Time in seconds')
 plt.title('Time in seconds LOOP vs Number of threads')
+plt.xticks(num)
+
+# Show the speedup of each xticks
+for i, v in enumerate(time_loop):
+    # If i is power of 2
+    if i in {0,1,3,7,15}:
+        plt.text(num[i], v, str(round(time_loop[0]/v, 2)) + 'x', color='blue', fontweight='bold', position=(num[i]-2, v))
+
+# Show the efficiency of each xticks
+for i, v in enumerate(time_loop):
+    efficiency_loop.append(time_loop[0]/(v*num[i]))
+    # If i is power of 2
+    if i in {0,1,3,7,15}:
+        plt.text(num[i], v, str(round(time_loop[0]/(v*num[i]), 2)) + 'x', color='red', fontweight='bold')
 
 plt.savefig('lab3/figLoop.png')
+
+plt.clf()
+
+print(num)
+print(efficiency)
+
+# Plot efficiency list
+fig = plt.plot(num, efficiency)
+plt.xlabel('Number of threads')
+plt.ylabel('Efficiency')
+plt.title('Efficiency vs Number of threads')
+plt.xticks(num)
+
+plt.savefig('lab3/figEfficiency.png')
+
+plt.clf()
+
+# Plot efficiency list
+fig = plt.plot(num, efficiency_loop)
+plt.xlabel('Number of threads')
+plt.ylabel('Efficiency')
+plt.title('Efficiency LOOP vs Number of threads')
+plt.xticks(num)
+
+plt.savefig('lab3/figEfficiencyLoop.png')
