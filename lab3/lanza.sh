d3f6b9308da76c6634e94fedacd42e398852d42f
ajtarraga <antoniojoaquin231999@gmail.com>
2023-03-30 18:08:42 +0200

    Media

--- lab3/lanza.sh
+++ lab3/lanza.sh
@@ -2,9 +2,12 @@
 
 gcc -fopenmp lab3_automatizar.c -o lab3.o
 
-# Do a for loop from 1 to 20
-for i in {1..20}
+for j in {1::10}
 do
-    # Launch the program with 4 million iterations and the current value of i+1
-    ./lab3.o 4000000 $i
+    # Do a for loop from 1 to 20
+    for i in {1..20}
+    do
+        # Launch the program with 4 million iterations and the current value of i+1
+        ./lab3.o 4000000 $i
+    done
 done
