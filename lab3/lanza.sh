bdcb1b9d701ea26fd6275a90432dc425125679c9
ajtarraga <antoniojoaquin231999@gmail.com>
2023-03-30 17:51:41 +0200

    Script para automatizar

---
+++ lab3/lanza.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gcc -fopenmp lab3_automatizar.c -o lab3.o
+
+# Do a for loop from 1 to 20
+for i in {1..20}
+do
+    # Launch the program with 4 million iterations and the current value of i+1
+    ./lab3.o 4000000 $i
+done
